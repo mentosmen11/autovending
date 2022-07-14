97cbb1c7b975bbca622ee8e67a0b379230200624
mentosmen11 <80728864+mentosmen11@users.noreply.github.com>
2022-07-15 01:34:58 +0900

    Delete tools directory

--- tools/Form.py
+++
@@ -1,5 +0,0 @@
-from flask_wtf import Form
-
-
-class PostForm(Form):
-    pass
--- tools/__init__.py
+++
@@ -1,9 +0,0 @@
-
-from .Database import database
-from .Session import session
-from .Random import random
-from .Site import site
-from .User import user
-from .Form import PostForm
-from .Product import product
-from .Coupon import coupon
