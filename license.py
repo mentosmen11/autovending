d157e036af8f6f88529efe940c26c381d40a0549
mentosmen11 <80728864+mentosmen11@users.noreply.github.com>
2022-07-15 01:35:45 +0900

    Delete license.py

--- license.py
+++
@@ -1,9 +0,0 @@
-from tools import database, random
-
-c = int(input("How Much: "))
-d = int(input("Day: "))
-
-for i in range(c):
-    code = random.genToken(7)
-    database.insert("license", code=code, day=d)
-    print(f"[{d}Day] Code: {code}")
